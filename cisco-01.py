80b9423a9df86d6fac1e3aefa274b203da873efe
Dave <davescripting@gmail.com>
2015-12-15 02:36:49 -0500

    Added comments and a bit of clarification on what each section does

--- cisco-01.py
+++ cisco-01.py
@@ -1,36 +1,50 @@
+'''
+Requires Python 2.7 and Paramiko to be installed
+
+
+'''
+
 import paramiko
 import time
 
+
+#Sets up the ssh session and logs in as user "dave" with password "password"
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh.connect('192.168.1.21', username='dave', password='password')
+
+
+#Creates a channel object, to overcome weirdness with cisco devices
 chan = ssh.invoke_shell()
 
-
+#Sends the 'enable' command to enter privilidged mode.
+#Note the '\n' after the command, this is equivalent to pressing "Enter".
 chan.send("enable\n")
 time.sleep(1)
-#resp = chan.recv(9999)
+chan.recv(9999)
 
-#print resp 
-
+#Sends the password, in this case, it's "password"
+#Also note that '\n' is there at the end, you should realize we need this after every command.
 chan.send("password\n")
 time.sleep(1)
-#resp = chan.recv(9999)
+chan.recv(9999)
 
-print resp
-
+#Disables paging by issuing the "terminal length 0" command.
+#I've added '\n' after the command.
 chan.send("terminal length 0\n")
 time.sleep(1)
-#resp = chan.recv(9999)
+chan.recv(9999)
 
-#print resp
-
+#In this case we want to see the configured vlans, and store the response.
 chan.send("show vlan brief\n")
 time.sleep(1)
 resp = chan.recv(9999)
 
-if "VLAN" in resp:
-    print "yes" 
-    for line in resp:
-        if "active" in line:
-            print line
+#We should have received a contiguous string of characters. 
+#Let's now split it apart into distinct lines
+
+resp = resp.split('\n')
+
+#Added as an example to print each line individually
+for line in resp:
+    print line
